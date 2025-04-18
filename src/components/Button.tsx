941925a383ff8383870786a6142839035be22db1
Tucker Mastin <quartalcounterpoint@gmail.com>
2025-04-17 21:42:58 -0700

    added react node

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 "use client"
+import {ReactNode} from "react";
 
-export function Button(props) {
+export function Button(props: {onClick:() => void; children: ReactNode}) {
     const onClick = props.onClick;
     const children = props.children;
     return <button onClick={onClick}>{children}</button>
